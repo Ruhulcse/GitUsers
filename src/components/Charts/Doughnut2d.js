155dfdedabe92c0bd8edf5d7b6a03a5253ec73d8
Ruhulcse <ruhul.cse7862@gmail.com>
2020-11-02 10:56:02 +0600

    Most star for a user

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,48 @@
-import React from 'react';
+// STEP 1 - Include Dependencies
+// Include react
+import React from "react";
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
-};
 
-export default Doughnut2d;
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Chart from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+// STEP 2 - Chart Data
+
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+
+const ChartComponent = ({data}) =>{
+  const chartConfigs = {
+    type: "doughnut2d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+       caption: 'Stars per language',
+       decimals:0,
+       doughnutRadius: '45%',
+       showPercentValues: 0,
+       theme: 'candy',
+      },
+      // Chart Data
+      data,
+    }
+  };
+  return <ReactFC {...chartConfigs} />;
+}
+
+export default ChartComponent;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,23 +4,31 @@ import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
  const { repos } = React.useContext(GithubContext);
- let languages = repos.reduce((total,item)=>{
-   const {language} = item;
+ const languages = repos.reduce((total,item)=>{
+   const {language, stargazers_count} = item;
    if(!language) return total;
    if(!total[language]){
-     total[language] = { label: language, value: 1 };
+     total[language] = { label: language, value: 1, stars: stargazers_count };
    }
    else{
      total[language] = {
        ...total[language],
        value: total[language].value+1,
+       stars: total[language].stars + stargazers_count,
      }
    }
    return total;
  },{});
- languages = Object.values(languages).sort((a,b) =>{
+ //popluar language
+ const mostUsed = Object.values(languages).sort((a,b) =>{
    return b.value - a.value;
  });
+ //most start in repository
+ const mostStar = Object.values(languages).sort((a,b)=>{
+   return b.stars - a.stars;
+ }).map((item) =>{
+   return {...item, value: item.stars};
+ }).slice(0,5);
  console.log(languages);
  const chartData = [
   {
@@ -39,8 +47,9 @@ const Repos = () => {
  return (
    <section className='section'>
      <Wrapper className='section-center'>
-       {/* <ExampleChart data={chartData}/> */}
-       <Pie3D data={languages}></Pie3D>
+       <Pie3D data={mostUsed}></Pie3D>
+       <div></div>
+      <Doughnut2D data={mostStar}/>
      </Wrapper>
    </section>
  );
